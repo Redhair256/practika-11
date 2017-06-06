27bf4de26bb5e632a19b50f068569e6f3be3b421
Redhair <Redhair20@hotmail.com>
2017-06-06 11:38:05 +0300

    Убранны лишнии роуты.

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ Route::get('/logout', function () {
 
 
 
-Auth::routes();
+	//Auth::routes();
+    // Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
